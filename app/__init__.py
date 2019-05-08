9eb7dafd03a7fe0dbe25a601e576332f8b742cbe
bobby-mohanty <bobby.mohanty2345@gmail.com>
2019-05-08 22:20:44 +0530

    Added user authentication and session handling

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 print type(app)
@@ -8,5 +9,8 @@ app.config['SECRET_KEY'] = 'a21066141f99b5ca69be95cafa55b063'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+loginManager = LoginManager(app)
+loginManager.login_view = 'login'
+loginManager.login_message_category = 'info'
 
 from app import routes
